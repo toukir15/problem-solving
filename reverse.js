e893909474fdb620f77e3102256122fa4383cfea
Tanvir <toukir.developer.bd@gmail.com>
2023-07-27 21:33:00 +0600

    sd

--- reverse.js
+++ reverse.js
@@ -1,16 +1,21 @@
-
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
 
 // Example Input: "hello world" Example Output: "dlrow olleh"
 
-const reverse = string => {
-    const reverseString = string.split("").reverse().join("")
-    return reverseString;
+
+function reverseString(inputStr) {
+    let reverseStr = ""
+    for (let i = inputStr.length - 1; i >= 0; i--) {
+        reverseStr = reverseStr + inputStr.charAt(i)
+    }
+    console.log(reverseStr);
 }
 
-const string = "hello world"
-const result = reverse(string)
+const string = "sdfldjsortjwe9ior"
+const result = reverseString(string);
 console.log(result);
+
+
 
 
 
